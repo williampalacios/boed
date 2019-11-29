b760aa14e6a69548f085a10392d8ccab46f18ffe
williampalacios <mich.palacios.hdz@gmail.com>
2019-11-28 19:10:45 -0600

    solo mostrar ver carrito desde item detail si no esta vacio, commit 2

--- core/views.py
+++ core/views.py
@@ -129,7 +129,11 @@ class ItemDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         referer = self.request.META.get('HTTP_REFERER')
-        order_qs = Order.objects.filter(user=self.request.user, ordered=False)
+
+        order_qs = []
+        if self.request.user.is_authenticated:
+            order_qs = Order.objects.filter(user=self.request.user,
+                                            ordered=False)
 
         context['referer'] = referer
         context['request'] = self.request
